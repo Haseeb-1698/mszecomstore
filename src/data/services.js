154b9a9875e6ee4ddcf8659ebff435a3ab6f8b1c
i221698 <i221698@nu.edu.pk>
2026-01-05 17:47:35 +0000

    icons damn

--- src/data/services.js
+++ src/data/services.js
@@ -5,7 +5,7 @@ export const services = [
     description: 'Stream unlimited movies and TV shows.',
     longDescription: 'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price.',
     price: '$9.99/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg',
+    logo: '/icons/netflix.svg',
     pricingTiers: [
       { name: 'Basic', price: '$9.99/mo', quality: 'SD (480p)' },
       { name: 'Standard', price: '$15.49/mo', quality: 'HD (1080p)' },
@@ -18,7 +18,7 @@ export const services = [
     description: 'Music for everyone. Ad-free listening.',
     longDescription: 'Spotify is a digital music, podcast, and video service that gives you access to millions of songs and other content from creators all over the world. Basic functions are free, but you can also choose to upgrade to Spotify Premium.',
     price: '$10.99/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg',
+    logo: '/icons/spotify.svg',
     pricingTiers: [
       { name: 'Individual', price: '$10.99/mo', quality: '320kbps' },
       { name: 'Duo', price: '$14.99/mo', quality: '320kbps' },
@@ -31,7 +31,7 @@ export const services = [
     description: 'The world’s best creative apps and services.',
     longDescription: 'Adobe Creative Cloud is a set of applications and services from Adobe Inc. that gives subscribers access to a collection of software used for graphic design, video editing, web development, photography, along with a set of mobile applications and also some optional cloud services.',
     price: '$54.99/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Adobe_Creative_Cloud_Logo.svg',
+    logo: '/icons/adobe-creative-cloud.svg',
     pricingTiers: [
       { name: 'Photography (20GB)', price: '$9.99/mo', quality: 'N/A' },
       { name: 'All Apps', price: '$54.99/mo', quality: 'N/A' },
@@ -44,7 +44,7 @@ export const services = [
     description: 'The all-in-one workspace for your notes, tasks, wikis, and databases.',
     longDescription: 'Notion is an application that provides components such as notes, databases, kanban boards, wikis, calendars and reminders. Users can connect these components to create their own systems for knowledge management, note taking, data management, project management, among others.',
     price: '$8/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Notion-logo.svg',
+    logo: '/icons/notion.svg',
     pricingTiers: [
       { name: 'Plus', price: '$8/mo', quality: 'N/A' },
       { name: 'Business', price: '$15/mo', quality: 'N/A' },
@@ -57,7 +57,7 @@ export const services = [
     description: 'The collaborative interface design tool.',
     longDescription: 'Figma is a collaborative web application for interface design, with additional offline features enabled by desktop applications for macOS and Windows. The Figma mobile app for Android and iOS allows viewing and interacting with Figma prototypes in real-time on mobile and tablet devices.',
     price: '$12/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg',
+    logo: '/icons/figma.svg',
     pricingTiers: [
       { name: 'Professional', price: '$12/mo', quality: 'N/A' },
       { name: 'Organization', price: '$45/mo', quality: 'N/A' },
@@ -68,7 +68,7 @@ export const services = [
     name: 'Disney+',
     description: 'The streaming home of your favorite stories.',
     price: '$7.99/mo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Disney%2B_logo.svg',
+    logo: '/icons/disney-plus.svg',
     longDescription: 'Disney+ is the streaming home of your favorite stories from Disney, Pixar, Marvel, Star Wars, and National Geographic. Watch the latest releases, Original series and movies, classic films, and more.',
     pricingTiers: [
       { name: 'Basic (With Ads)', price: '$7.99/mo', quality: 'Up to 4K UHD' },
