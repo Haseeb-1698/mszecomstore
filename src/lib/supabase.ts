432458b010c64cb6ead5c74a055506663acf1ea5
Haseeb-1698 <I221698@nu.edu.pk>
2026-01-14 11:00:51 +0500

    typescript types resolved.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import type { Database } from './database.types'
+import type { Database } from './database.generated'
 
 // Get environment variables
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
